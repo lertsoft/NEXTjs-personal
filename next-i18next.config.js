4d67de2f089aa47e466d6843325d807acd466e19
Ronny Coste <ronny21.97@gmail.com>
2022-03-31 09:46:56 -0400

    see if that works

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,7 +2,7 @@ module.exports = {
     i18n: {
       defaultLocale: 'en',
       locales: ['en', 'es'],
-      localePath: path.resolve("./public/locales/"),
+      // localePath: path.resolve("./public/locales/"), Do not use it breaks the app
       // use: [initReactI18next],   // this line did not affect the error in any way
       debug: true,
     },
--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,23 @@ import "../styles/globals.css";
 import "../styles/scrollTop.css";
 import { ThemeProvider } from "next-themes";
 import { appWithTranslation } from 'next-i18next';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <ThemeProvider defaultTheme="dark" attribute="class">
       <Component {...pageProps} />
