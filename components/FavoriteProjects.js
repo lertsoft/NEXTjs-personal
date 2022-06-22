2d929586225203d1322a473fd15a925abae33533
Ronny Coste <ronny21.97@gmail.com>
2022-06-22 16:38:06 -0400

    Fixed the sizing and animations

--- components/FavoriteProjects.js
+++ components/FavoriteProjects.js
@@ -59,17 +59,17 @@ export default function FavouriteProjects() {
           {/* Single card */}
           <a
             href="https://lifecycle380.herokuapp.com"
-            className="w-full block col-span-3 sm:col-span-1  object-cover"
+            className="w-full block col-span-3  sm:col-span-2 shadow-2xl"
           >
-            <div className="relative overflow-hidden shadow-2xl">
+            <div className="relative overflow-hidden">
               {/* <div className="overlay absolute inset-0 bg-black bg-opacity-70 z-10"></div> */}
               <img
                 src="/lifecycle.jpg"
-                alt="portfolio"
-                className="transform hover:scale-125 transition duration-2000 ease-out object-cover shadow-2xl"
+                alt="Lifecycle recycling app"
+                className="transform hover:scale-125 transition duration-2000 ease-out"
               />
               <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
-              Lifecycle            
+              Lifecycle 
               </h1>
               <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
                 02
@@ -78,24 +78,26 @@ export default function FavouriteProjects() {
           </a>
           {/* Single card */}
           <a
-            href="https://en.ronnycoste.com"
-            className="w-full block col-span-3  sm:col-span-2 shadow-2xl"
+            href="https://typescript-nextjs-template.vercel.app"
+            className="w-full block col-span-3 sm:col-span-1  object-cover"
           >
-            <div className="relative overflow-hidden">
+            <div className="relative overflow-hidden shadow-2xl">
               {/* <div className="overlay absolute inset-0 bg-black bg-opacity-70 z-10"></div> */}
               <img
                 src="/Template.png"
                 alt="Template Site with my favorite tooling"
-                className="transform hover:scale-125 transition duration-2000 ease-out"
+                className="transform hover:scale-125 transition duration-2000 ease-out object-cover shadow-2xl"
               />
               <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
-              Template with tooling
+              Template with tooling           
               </h1>
               <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
                 03
               </h1>
             </div>
           </a>
+          
+          
           
           
         </div>
