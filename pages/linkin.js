40a2f71437982f93ecd351521276bab91c8d5387
Ronny Coste <ronny21.97@gmail.com>
2022-07-30 14:16:15 -0400

    added a link in bio component and page

---
+++ pages/linkin.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ContainerBlock from "../components/ContainerBlock";
+import Linkin from "../components/Linkin";
+// import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
+export default function projects() {
+  return (
+    // <ContainerBlock title="Link In Bio - Ronny Coste">
+      <Linkin />
+    // </ContainerBlock>
+  );
+}
